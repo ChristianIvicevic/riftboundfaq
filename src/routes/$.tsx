0f7f20982279d069fade0a8580d7f72ad0aa0779
Christian Ivicevic <mail@christian-ivicevic.com>
2026-01-06 00:47:30 +1100

    Add "Edit this page" footer link to DocsPage component

--- src/routes/$.tsx
+++ src/routes/$.tsx
@@ -31,9 +31,41 @@ const loader = createServerFn()
 
 const clientLoader = browserCollections.docs.createClientLoader({
 	id: 'docs',
-	component({ toc, frontmatter, default: MDX }) {
+	component({ toc, frontmatter, default: MDX }, props: { path: string }) {
 		return (
-			<DocsPage toc={toc}>
+			<DocsPage
+				toc={toc}
+				tableOfContent={{
+					footer: (
+						<div className="text-sm text-fd-muted-foreground mt-6">
+							<a
+								className="hover:text-fd-foreground transition-colors flex gap-2 items-center"
+								href={`https://github.com/ChristianIvicevic/riftboundfaq/blob/main/content/${props.path}`}
+								rel="noopener noreferrer"
+								target="_blank"
+							>
+								{/** biome-ignore lint/a11y/noSvgWithoutTitle: Ignore title */}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									className="size-4"
+								>
+									<path d="M13 21h8" />
+									<path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+								</svg>
+								Edit this page
+							</a>
+						</div>
+					),
+				}}
+			>
 				<DocsTitle>{frontmatter.title}</DocsTitle>
 				<DocsDescription>{frontmatter.description}</DocsDescription>
 				<DocsBody>
@@ -51,7 +83,7 @@ function Page() {
 
 	return (
 		<DocsLayout {...baseOptions()} tree={pageTree}>
-			<Content />
+			<Content path={data.path} />
 		</DocsLayout>
 	)
 }
