38ef862aa996e9fbcce23dbcd7e56375939b76eb
Christian Ivicevic <mail@christian-ivicevic.com>
2026-01-21 23:34:08 +1100

    Revert manual introduction of nitro

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 import mdx from 'fumadocs-mdx/vite'
-import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
@@ -12,8 +11,7 @@ export default defineConfig({
 		mdx(await import('./source.config')),
 		tailwindcss(),
 		tsConfigPaths({ projects: ['./tsconfig.json'] }),
-		tanstackStart(),
-		nitro(),
+		tanstackStart({ prerender: { enabled: true } }),
 		react(),
 	],
 })
