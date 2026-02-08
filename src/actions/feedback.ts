657706c52ef79a42948923960d9cae6a5c690aa1
Christian Ivicevic <mail@christian-ivicevic.com>
2026-02-08 23:21:06 +1100

    Refactor feedback actions to reuse analytics event logic

--- src/actions/feedback.ts
+++ src/actions/feedback.ts
@@ -4,7 +4,10 @@ import { PostHog } from 'posthog-node'
 import { type ActionResponse, BlockFeedback, PageFeedback } from '@/components/feedback/schema'
 import { env } from '@/env'
 
-export async function submitPageFeedback(feedback: PageFeedback): Promise<ActionResponse> {
+async function captureAnalyticsEvent(
+	eventName: string,
+	properties: Record<string, unknown>,
+): Promise<ActionResponse> {
 	if (!env.POSTHOG_API_KEY) {
 		console.warn('POSTHOG_API_KEY is missing. Feedback analytics will not be captured.')
 		return {}
@@ -12,22 +15,21 @@ export async function submitPageFeedback(feedback: PageFeedback): Promise<Action
 
 	const client = new PostHog(env.POSTHOG_API_KEY, { host: 'https://us.i.posthog.com' })
 	client.capture({
-		event: 'page feedback submitted',
-		properties: { $process_person_profile: false, ...feedback },
+		event: eventName,
+		properties: {
+			$process_person_profile: false,
+			...properties,
+			isProduction: process.env.NODE_ENV === 'production',
+		},
 	})
+	await client.shutdown()
 	return {}
 }
 
-export async function submitBlockFeedback(feedback: BlockFeedback): Promise<ActionResponse> {
-	if (!env.POSTHOG_API_KEY) {
-		console.warn('POSTHOG_API_KEY is missing. Feedback analytics will not be captured.')
-		return {}
-	}
+export async function submitPageFeedback(feedback: PageFeedback): Promise<ActionResponse> {
+	return captureAnalyticsEvent('page feedback submitted', feedback)
+}
 
-	const client = new PostHog(env.POSTHOG_API_KEY, { host: 'https://us.i.posthog.com' })
-	client.capture({
-		event: 'block feedback submitted',
-		properties: { $process_person_profile: false, ...feedback },
-	})
-	return {}
+export async function submitBlockFeedback(feedback: BlockFeedback): Promise<ActionResponse> {
+	return captureAnalyticsEvent('block feedback submitted', feedback)
 }
