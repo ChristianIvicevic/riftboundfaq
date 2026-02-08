a3378e9e36ba29454ca24dd057f96733af1a7174
Christian Ivicevic <mail@christian-ivicevic.com>
2026-02-08 22:50:23 +1100

    Integrate @t3-oss/env-nextjs

--- src/app/(wiki)/[[...slug]]/actions.ts
+++ src/app/(wiki)/[[...slug]]/actions.ts
@@ -2,11 +2,15 @@
 
 import { PostHog } from 'posthog-node'
 import { type ActionResponse, BlockFeedback, PageFeedback } from '@/components/feedback/schema'
+import { env } from '@/env'
 
 export async function submitPageFeedback(feedback: PageFeedback): Promise<ActionResponse> {
-	if (!process.env.POSTHOG_API_KEY) return {}
+	if (!env.POSTHOG_API_KEY) {
+		console.warn('POSTHOG_API_KEY is missing. Feedback analytics will not be captured.')
+		return {}
+	}
 
-	const client = new PostHog(process.env.POSTHOG_API_KEY, { host: 'https://us.i.posthog.com' })
+	const client = new PostHog(env.POSTHOG_API_KEY, { host: 'https://us.i.posthog.com' })
 	client.capture({
 		event: 'page feedback submitted',
 		properties: { $process_person_profile: false, ...feedback },
@@ -15,9 +19,12 @@ export async function submitPageFeedback(feedback: PageFeedback): Promise<Action
 }
 
 export async function submitBlockFeedback(feedback: BlockFeedback): Promise<ActionResponse> {
-	if (!process.env.POSTHOG_API_KEY) return {}
+	if (!env.POSTHOG_API_KEY) {
+		console.warn('POSTHOG_API_KEY is missing. Feedback analytics will not be captured.')
+		return {}
+	}
 
-	const client = new PostHog(process.env.POSTHOG_API_KEY, { host: 'https://us.i.posthog.com' })
+	const client = new PostHog(env.POSTHOG_API_KEY, { host: 'https://us.i.posthog.com' })
 	client.capture({
 		event: 'block feedback submitted',
 		properties: { $process_person_profile: false, ...feedback },
