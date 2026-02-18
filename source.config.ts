9fed709ec2ca4c5eb9e98d39542bb5b7721a7afd
Christian Ivicevic <mail@christian-ivicevic.com>
2026-02-18 21:41:00 +1100

    Configure `remarkFeedbackBlock` to use custom options for processing feedback blocks

--- source.config.ts
+++ source.config.ts
@@ -1,4 +1,4 @@
-import { remarkFeedbackBlock } from 'fumadocs-core/mdx-plugins'
+import { remarkFeedbackBlock, RemarkFeedbackBlockOptions } from 'fumadocs-core/mdx-plugins'
 import { defineConfig, defineDocs, frontmatterSchema, metaSchema } from 'fumadocs-mdx/config'
 import lastModified from 'fumadocs-mdx/plugins/last-modified'
 import { z } from 'zod'
@@ -15,7 +15,22 @@ export const docs = defineDocs({
 	meta: { schema: metaSchema },
 })
 
+const feedbackBlockOptions: RemarkFeedbackBlockOptions = {
+	resolve(node) {
+		switch (node.type) {
+			case 'mdxJsxFlowElement':
+				return node.name === 'Callout' ? true : 'skip'
+			case 'paragraph':
+			case 'image':
+			case 'listItem':
+				return true
+			default:
+				return false
+		}
+	},
+}
+
 export default defineConfig({
 	plugins: [lastModified()],
-	mdxOptions: { remarkPlugins: [remarkFeedbackBlock] },
+	mdxOptions: { remarkPlugins: [[remarkFeedbackBlock, feedbackBlockOptions]] },
 })
