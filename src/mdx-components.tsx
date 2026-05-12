b032ed883e1baeca8c6a89b86c120d58dec1895d
Christian Ivicevic <mail@christian-ivicevic.com>
2026-05-12 21:49:07 +1000

    Add new Card component

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,10 +2,12 @@ import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
 import { submitBlockFeedback } from '@/actions/feedback'
 import { FeedbackBlock } from '@/components/feedback/client'
+import { Card } from '@/components/cards/card'
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
 	return {
 		...defaultMdxComponents,
+		Card,
 		FeedbackBlock: (props) => <FeedbackBlock {...props} onSendAction={submitBlockFeedback} />,
 		...components,
 	}
