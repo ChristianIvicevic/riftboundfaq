173b332de8111feb5346a37e346906cbaddf8ded
Christian Ivicevic <mail@christian-ivicevic.com>
2026-07-19 16:10:06 +1000

    Add cards and rules clarifications for Shadowblade Lurker, Consuming Curse, and Shadow Assassin; integrate Vendetta set

--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -2,10 +2,12 @@ import { ORIGINS_CARDS } from '@/components/cards/origins'
 import { PROVING_GROUNDS_CARDS } from '@/components/cards/proving-grounds'
 import { SPIRITFORGED_CARDS } from '@/components/cards/spiritforged'
 import { UNLEASHED_CARDS } from '@/components/cards/unleashed'
+import { VENDETTA_CARDS } from '@/components/cards/vendetta'
 
 const GALLERY_BASE = 'https://playriftbound.com/en-us/card-gallery/'
 
 const SETS = [
+	{ id: 'vendetta', cards: VENDETTA_CARDS as Record<string, string> },
 	{ id: 'unleashed', cards: UNLEASHED_CARDS as Record<string, string> },
 	{ id: 'spiritforged', cards: SPIRITFORGED_CARDS as Record<string, string> },
 	{ id: 'origins', cards: ORIGINS_CARDS as Record<string, string> },
