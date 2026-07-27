1ab3c7e02a8cb68499d4ef3af773613da5b6603e
Christian Ivicevic <mail@christian-ivicevic.com>
2026-07-27 14:19:52 +1000

    Refactor and relocate feedback actions and components to `features/feedback` directory, updating imports accordingly

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,16 +1,16 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
 import type { ComponentProps } from 'react'
-import { submitBlockFeedback } from '@/actions/feedback'
 import { Card } from '@/components/cards/card'
 import { CoreRulesDiff } from '@/components/core-rules/diff-view'
 import { Rule } from '@/components/core-rules/rule'
 import { CoreRulesTable } from '@/components/core-rules/table'
-import { FeedbackBlock } from '@/components/feedback/client'
 import { MDX_TERMS } from '@/components/game-terms'
 import { Energy, RUNES, Universal } from '@/components/resources'
 import { TournamentRulesDiff } from '@/components/tournament-rules/diff-view'
 import { TournamentRulesTable } from '@/components/tournament-rules/table'
+import { submitBlockFeedback } from '@/features/feedback/actions'
+import { FeedbackBlock } from '@/features/feedback/feedback'
 
 const wikiMdxComponents = {
 	...defaultMdxComponents,
